a20514f16af47abcc8a7830c05633bed15056d6f
byteme8bit <lynchpcconsulting@gmail.com>
2018-05-27 12:35:59 -0700

    First commit:
    Just initial sketch of app

---
+++ program.py
@@ -0,0 +1,29 @@
+__author__ = "byteme8bit"
+
+
+def main():
+    pass
+
+
+def print_header():
+    pass
+
+
+def get_folder_from_user():
+    pass
+
+
+def get_search_text_from_user():
+    pass
+
+
+def search_folders():
+    pass
+
+
+def search_files():
+    pass
+
+
+if __name__ == '__main__':
+    main()
